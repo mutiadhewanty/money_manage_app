bbb10efeee56e14c9e8328cf5df9125f8082eded
mutiadhewanty <mutiadhewanty@gmail.com>
2022-06-20 08:53:15 +0700

    hide screen

--- lib/screen/pengeluaran.dart
+++ lib/screen/pengeluaran.dart
@@ -17,18 +17,15 @@ class PengeluaranScreen extends StatefulWidget {
 class _PengeluaranScreenState extends State<PengeluaranScreen> {
   // Tanggal Sekarang
   static var today = new DateTime.now();
-  var formatedTanggal = new DateFormat.yMMMd().format(today);
+  var formatedTanggal = DateFormat('yyyy/MM/dd').format(today);
 
   // Action
   var userInput = '';
-  var answer = '0';
-
+  bool showKeyboard=true;
+  bool showList=false;
+  
   // kalkulator
   final List<String> buttons = [
-    // 'C',
-    // '+/-',
-    // '%',
-    // 'DEL',
     '7',
     '8',
     '9',
@@ -117,169 +114,148 @@ class _PengeluaranScreenState extends State<PengeluaranScreen> {
                           icon: Icon(Icons.delete, color: Colors.green.shade900,)),
                     ],
                   ),
-                  Container(
-                      padding: EdgeInsets.all(10),
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        answer,
-                        style: TextStyle(fontSize: 20),
-                      )),
+                  
                 ],
               ),
-              // padding: EdgeInsets.all(7.0),
-              // margin: EdgeInsets.all(7.0),
-              // child: TextField(
-              //   decoration: InputDecoration(
-              //       filled: true,
-              //       fillColor: Color.fromARGB(255, 195, 238, 145),
-                    // suffixIcon: Icon(Icons.delete_forever),
-              //       border: OutlineInputBorder(
-              //         borderRadius: BorderRadius.circular(9),
-              //         borderSide: BorderSide.none,
-              //       )),
-              // ),
             ),
             SizedBox(
               height: 10,
             ),
-            Container(
-                // padding: EdgeInsets.all(7.0),
-                // margin: EdgeInsets.all(7.0),
-                // child: TextField(
-                //   decoration: InputDecoration(
-                //       enabled: false,
-                //       hintText: 'hint',
-                //       helperText: 'helper',
-                //       labelText: 'Note',
-                //       prefixIcon: Icon(Icons.pending_actions_outlined),
-                //       counterText: 'counter'),
-                // ),
-                ),
-
-            // Expanded(
-            //     flex: 4,
-            //     child: Container(
-            //       child: Center(
-            //         child: GridView.builder(
-            //             itemCount: buttons.length,
-            //             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            //                 crossAxisCount: 4),
-            //             itemBuilder: (BuildContext context, int index) {
-            //               if (index == 15) {
-            //                 return ButtonsPengeluaran(
-            //                   buttonText: buttons[index],
-            //                   color: Colors.blue,
-            //                   textColor: Colors.white,
-            //                 );
-            //               }
-            //               // tanda =
-            //               else if (index == buttons.length - 1) {
-            //                 return ButtonsPengeluaran(
-            //                   buttonTapped: () {
-            //                     setState(() {
-            //                       equalPressed();
-            //                     });
-            //                   },
-            //                   buttonText: buttons[index],
-            //                   color: Colors.lightGreen,
-            //                   textColor: Colors.white,
-            //                 );
-            //               } else {
-            //                 return ButtonsPengeluaran(
-            //                   buttonTapped: () {
-            //                     setState(() {
-            //                       userQuestion += buttons[index];
-            //                     });
-            //                   },
-            //                   buttonText: buttons[index],
-            //                   color: isOperator(buttons[index])
-            //                       ? Colors.lightGreen
-            //                       : Colors.lightGreen,
-            //                   textColor: isOperator(buttons[index])
-            //                       ? Colors.white
-            //                       : Colors.white,
-            //                 );
-            //               }
-            //             }),
-            //       ),
-            //     )),
-
-            Expanded(
-            flex: 3,
-            child: Container(
-              child: GridView.builder(
-                  itemCount: buttons.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4),
-                  itemBuilder: (BuildContext context, int index) {
-                    
-                    // Equal_to Button
-                    if (index == 14) {
-                      return ButtonsPengeluaran(
-                        buttonTapped: () {
-                          setState(() {
-                            equalPressed();
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: Colors.orange[700],
-                        textColor: Colors.white,
-                      );
-                    }
- 
-                    //  other buttons
-                    else {
-                      return ButtonsPengeluaran(
-                        buttonTapped: () {
-                          setState(() {
-                            userInput += buttons[index];
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: isOperator(buttons[index])
-                            ? Colors.blueAccent
-                            : Colors.white,
-                        textColor: isOperator(buttons[index])
-                            ? Colors.white
-                            : Colors.black,
-                      );
-                    }
-                  }), // GridView.builder
-            ),
-          ),
+            expenseList(),
+            // showKeyboard?keyboard():Container(),
+            // showList?Container( 
+            //          height: 150,
+            //          color: Colors.lightBlue,
+            //       ):Container(),
+            Divider(),
             // choose category
-            // Container(
-            //   margin: EdgeInsets.all(10),
-            //   height: 50.0,
-            //   width: 200.0,
-            //   child: RaisedButton(
-            //     shape: RoundedRectangleBorder(
-            //         borderRadius: BorderRadius.circular(7.0),
-            //         side: BorderSide(color: Color.fromARGB(255, 146, 240, 24))),
-            //     onPressed: () {},
-            //     padding: EdgeInsets.all(10.0),
-            //     color: Colors.white,
-            //     textColor: Color.fromARGB(255, 39, 219, 22),
-            //     child: Text("Choose Category", style: TextStyle(fontSize: 15)),
-            //   ),
-            // ),
-            Center(
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: const Color.fromARGB(255, 25, 27, 193),
-                          padding: EdgeInsets.only(
-                              left: 80, right: 80, top: 15, bottom: 15)),
-                      child: const Text(
-                        'Submit',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      onPressed: () {
-                        saveOrder();
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 50.0,
+              width: 200.0,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7.0),
+                    side: BorderSide(color: Color.fromARGB(255, 146, 240, 24))),
+                onPressed: () {setState(() {
+                  showKeyboard = false;
+                  showList = true;
+                });},
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                textColor: Color.fromARGB(255, 39, 219, 22),
+                child: Text("Choose Category", style: TextStyle(fontSize: 15)),
+              ),
+            ),
+            // Center(
+            //       child: ElevatedButton(
+            //           style: ElevatedButton.styleFrom(
+            //               primary: const Color.fromARGB(255, 25, 27, 193),
+            //               padding: EdgeInsets.only(
+            //                   left: 80, right: 80, top: 15, bottom: 15)),
+            //           child: const Text(
+            //             'Submit',
+            //             style: TextStyle(fontSize: 15),
+            //           ),
+            //           onPressed: () {
+            //             saveOrder();
                         
-                      }),
-                )
+            //           }),
+            //     )
           ],
         ));
+  }
+
+  Expanded expenseList() {
+    return Expanded(
+        flex: 3,
+        child: Container(
+          child: GridView.builder(
+              itemCount: buttons.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3),
+              itemBuilder: (BuildContext context, int index) {
+                
+                // Equal_to Button
+                if (index == 14) {
+                  return ButtonsPengeluaran(
+                    buttonTapped: () {
+                      setState(() {
+                        equalPressed();
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: Color.fromARGB(255, 29, 103, 141),
+                    textColor: Colors.white,
+                  );
+                }
+ 
+                //  other buttons
+                else {
+                  return ButtonsPengeluaran(
+                    buttonTapped: () {
+                      setState(() {
+                        userInput += buttons[index];
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: isOperator(buttons[index])
+                        ? Colors.green.shade700
+                        : Colors.green.shade300,
+                    textColor: isOperator(buttons[index])
+                        ? Colors.white
+                        : Colors.white,
+                  );
+                }
+              }), // GridView.builder
+        ),
+      );
+  }
+
+  Expanded keyboard() {
+    return Expanded(
+          flex: 3,
+          child: Container(
+            child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  
+                  // Equal_to Button
+                  if (index == 14) {
+                    return ButtonsPengeluaran(
+                      buttonTapped: () {
+                        setState(() {
+                          equalPressed();
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Color.fromARGB(255, 29, 103, 141),
+                      textColor: Colors.white,
+                    );
+                  }
+ 
+                  //  other buttons
+                  else {
+                    return ButtonsPengeluaran(
+                      buttonTapped: () {
+                        setState(() {
+                          userInput += buttons[index];
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: isOperator(buttons[index])
+                          ? Colors.green.shade700
+                          : Colors.green.shade300,
+                      textColor: isOperator(buttons[index])
+                          ? Colors.white
+                          : Colors.white,
+                    );
+                  }
+                }), // GridView.builder
+          ),
+        );
   }
 
   bool isOperator(String x) {
@@ -291,12 +267,16 @@ class _PengeluaranScreenState extends State<PengeluaranScreen> {
 
   void equalPressed() {
     String finalQuestion = userInput;
+    finalQuestion = userInput.replaceAll('x', '*');
 
     Parser p = Parser();
     Expression exp = p.parse(finalQuestion);
     ContextModel cm = ContextModel();
     double eval = exp.evaluate(EvaluationType.REAL, cm);
 
-    answer = eval.toString();
+    userInput = eval.toString();
+  }
+  void SampleContainer(){
+    var sts = "empty";
   }
 }
